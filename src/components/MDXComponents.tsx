e318d107f2e1c013c3b5bd16778e2e066ac3fc8d
Matei Canavra <tools@matei.work>
2025-05-18 15:43:33 -0400

    style: align theme with encore design

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -17,8 +17,8 @@ export const components = {
   FilterableProjectGrid: FilterableProjectGrid,
   
   // Override default elements
-  h1: (props: any) => <h1 className="text-4xl font-extrabold mt-8 mb-4" {...props} />,
-  h2: (props: any) => <h2 className="text-3xl font-bold mt-8 mb-3" {...props} />,
+  h1: (props: any) => <h1 className="text-5xl font-extrabold mt-8 mb-4" {...props} />,
+  h2: (props: any) => <h2 className="text-4xl font-bold mt-8 mb-3" {...props} />,
   h3: (props: any) => <h3 className="text-2xl font-semibold mt-6 mb-3" {...props} />,
   h4: (props: any) => <h4 className="text-xl font-semibold mt-4 mb-2" {...props} />,
   
